839149359544b52fbba8508071165898ea2a653e
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-21 13:10:51 -0700

    LODASH

--- src/Lodash2.tsx
+++ src/Lodash2.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import _ from 'lodash'
 
+//TYPES 
+type ProductType = { id: number, title: string, price: number, isAvailable: boolean }
+
+
+
+
+
+// COMPONENT
 const Lodash2 = () => {
+  //STATES
+  const [products, setProducts] = useState([
+    { id: 1, title: "MacBook Pro", price: 20_000_000, isAvailable: true },
+    { id: 2, title: "MacBook Cover", price: 500_000, isAvailable: false },
+    { id: 3, title: "MacBook Bag", price: 700_000, isAvailable: true },
+  ])
+
+
+
+
   return (
-    <div>Lodash2</div>
+    <div className='text-white'>
+      Lodash2
+    </div>
   )
 }
 

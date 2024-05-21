04c789b210b01807e65741437c41dd910c7a4ef6
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-21 12:39:37 -0700

    LODASH

--- src/Loadash.tsx
+++ src/Loadash.tsx
@@ -29,29 +29,29 @@ const Loadash = () => {
       <h1 className="text-slate-100 text-3xl font-bold bg-pink-500 p-4 text-center border-b-4 border-sky-300">LODASH PAGE</h1>
       <hr />
 
-      {/*//^ SUM */}
+      {/* SUM */}
       <h2 className='text-white text-3xl font-bold px-8 py-6 bg-zinc-800 border-b-4 border-red-600'>Sum : {Lodash.sum([1, 2, 3, 4])}</h2>
 
-      {/*//^ SUMBY */}
+      {/* SUMBY */}
       <h2 className='text-white text-3xl font-bold px-8 py-6 bg-zinc-800 border-b-4 border-yellow-400'>Total Price = <span className="text-5xl text-blue-400">{Lodash.sumBy(products, product => product.price).toLocaleString()}</span> <span className="text-xl text-pink-500">Tomans</span> </h2>
 
 
-      {/*//^ FILTER */}
+      {/* FILTER */}
       <h2 className='text-white text-3xl font-bold px-8 py-6 bg-zinc-800 border-b-4 border-blue-500 flex'> Filtered items : <span className="pl-2 flex text-purple-600 text-5xl">   {Lodash.filter(products, (product) => product.price > 300_000).map(item => <p className="px-2">{item.title} /</p>)}  </span></h2>
 
       <h2 className='text-white text-3xl font-bold px-8 py-6 bg-zinc-800 border-b-4 border-lime-500 flex items-center'> Filtered items 2 :<span className="pl-2 flex text-cyan-400 text-5xl">  {Lodash.filter(products, { title: "Hub" }).map(item => <p className="px-2">{item.title}</p>)}   </span></h2>
 
       <h2 className='text-white text-3xl font-bold px-8 py-6 bg-zinc-800 border-b-4 border-pink-500 flex items-center'> Filtered items 3 :<span className="pl-2 flex text-red-600 text-5xl">  {Lodash.filter(products, "count").map(item => <p className="px-2">{item.title} /</p>)}   </span></h2>
 
-      {/*//^ MAP */}
+      {/* MAP */}
       <h2 className='text-white text-3xl font-bold px-8 py-6 bg-zinc-800 border-b-4 border-white flex items-center'>
         {Lodash.map(products, product => { product.price += 1_000_000; return product })
           .map((product: ProductType) => <p className="px-2 text-lime-400 text-4xl">  {product.title} : {product.price.toLocaleString()} <span className="text-red-700">/</span> </p>)}
       </h2>
 
-      {/*//^ PICK */}
+      {/* PICK */}
       <h2 className='text-white text-3xl font-bold px-8 py-6 bg-zinc-800 border-b-4 border-violet-600 flex items-center'>Lodash Pick : {Lodash.pick(LodashPickObj, ['a', 'b']).a} </h2>
-      {/*//^ PICKBY */}
+      {/* PICKBY */}
       <h2 className='text-white text-3xl font-bold px-8 py-6 bg-zinc-800 border-b-4 border-violet-600 flex items-center'>Lodash PickBy : {Lodash.pickBy(LodashPickObj, Lodash.isNumber).b} </h2>
 
     </>

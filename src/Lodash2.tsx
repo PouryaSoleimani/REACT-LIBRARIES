498b486b2d5239b302e57e314c50f035aaa44d58
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-21 13:50:06 -0700

    LODASH

--- src/Lodash2.tsx
+++ src/Lodash2.tsx
@@ -4,7 +4,7 @@ import _ from 'lodash'
 //TYPES 
 type ProductType = { id: number, title: string, price: number, isAvailable: boolean }
 type ItemType = number | ProductType
-// COMPONENT
+//^ COMPONENT
 const Lodash2 = () => {
   //STATES
   const [products, setProducts] = useState([
@@ -12,10 +12,7 @@ const Lodash2 = () => {
     { id: 2, title: "MacBook Cover", price: 500_000, isAvailable: false },
     { id: 3, title: "MacBook Bag", price: 700_000, isAvailable: true },
   ])
-
-
-
-
+  //RETURN
   return (
     <div className='text-white'>
       {/* SUM */}
@@ -23,7 +20,12 @@ const Lodash2 = () => {
       {/* SUMBY */}
       <h1 className='text-6xl font-bold p-6 bg-zinc-800'>{_.sumBy(products, (product) => product.price).toLocaleString()} Tomans</h1>
       {/* FILTER */}
-      <h1 className='text-3xl flex items-center justify-start space-x-6 font-bold p-6 bg-zinc-700'>{_.filter(products, (product) => { product.price * 2; return product }).map((item) => <p>{item.id}.{item.title}</p>)}</h1>
+      <h1 className='text-3xl flex items-center justify-start space-x-6 font-bold p-6 bg-zinc-700'>{_.filter(products, (product) => { product.price * 2; return product }).map((item) => <p>{item.id}.{item.title} / {item.price.toLocaleString()}</p>)}</h1>
+      {/* FILTER 2 */}
+      <h1 className='text-3xl flex items-center justify-start space-x-6 font-bold p-6 bg-zinc-600'>{_.filter(products, { id: 2 }).map(item => <p>{item.id}.{item.title}</p>)}</h1>
+      {/* FILTER 3 */}
+      <h1 className='text-3xl flex items-center justify-start space-x-6 font-extrabold p-6 bg-zinc-500 text-black'>{_.filter(products, "isAvailable").map(item => <p>{item.title} </p>)}</h1>
+      
     </div>
   )
 }

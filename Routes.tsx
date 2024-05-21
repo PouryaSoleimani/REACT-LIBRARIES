8213866c6634435943ef4ba3c58f265fb6fdaff9
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-21 06:06:23 -0700

    LOADASH

--- Routes.tsx
+++ Routes.tsx
@@ -1,7 +1,11 @@
 //^ ROUTES
 import { createBrowserRouter } from "react-router-dom";
 import Home from './src/App'
+import Loadash from "./src/Loadash";
 
+
+// * ROUTER
 export const router = createBrowserRouter([
-  { path: '/', element: <Home /> }
+  { path: '/', element: <Home /> },
+  { path: '/loadash', element: <Loadash /> },
 ])
--- src/Loadash.tsx
+++ src/Loadash.tsx
@@ -2,7 +2,7 @@
 
 const Loadash = () => {
   return (
-    <div>Loadash</div>
+    <h1 className='bg-pink-900 p-4 text-center w-full font-bold text-5xl text-slate-100 border-b-8 border-black '>LOADASH PAGE</h1>
   )
 }
 

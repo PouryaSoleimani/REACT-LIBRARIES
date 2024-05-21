97e8f1cae89063276eed69a0f3b910368e8c7d06
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-21 14:35:48 -0700

    LODASH

--- src/Lodash2.tsx
+++ src/Lodash2.tsx
@@ -17,18 +17,25 @@ const Lodash2 = () => {
     <div className='text-white'>
       {/*SUM */}
       <h1 className='text-6xl font-bold p-6 bg-zinc-900'> SUM : {_.sum([1, 2, 3, 4, 5])} </h1>
+
       {/*SUMBY */}
       <h1 className='text-6xl font-bold p-6 bg-zinc-800'>{_.sumBy(products, (product) => product.price).toLocaleString()} Tomans</h1>
+
       {/*FILTER */}
       <h1 className='text-3xl flex items-center justify-start space-x-6 font-bold p-6 bg-zinc-700'>{_.filter(products, (product) => { product.price > 700_000; return product }).map(item => <p>{item.id}.{item.title} / {item.price.toLocaleString()}</p>)}</h1>
+
       {/*FILTER 2 */}
       <h1 className='text-3xl flex items-center justify-start space-x-6 font-bold p-6 bg-zinc-600'>{_.filter(products, { id: 2 }).map(item => <p>{item.id}.{item.title}</p>)}</h1>
+
       {/*FILTER 3 */}
       <h1 className='text-3xl flex items-center justify-start space-x-6 font-extrabold p-6 bg-zinc-500 text-black'>{_.filter(products, "isAvailable").map(item => <p>{item.title} </p>)}</h1>
+
       {/*STRING CAPITALIZE */}
       <h1 className='text-3xl flex items-center justify-start space-x-6 font-extrabold p-6 bg-zinc-400 text-black'>{_.capitalize('POURYA')}</h1>
+
       {/*STRING CAMELCASE */}
       <h1 className='text-3xl flex items-center justify-start space-x-6 font-extrabold p-6 bg-zinc-300 text-black'>{_.camelCase('add todo function')}</h1>
+
       {/*ARRAY CHUNK */}
       <h1 className='text-3xl flex items-center justify-start space-x-6 font-extrabold p-6 bg-zinc-200 text-black'>{_.chunk(["1", "2", "3", "4", "5", "6"], 4)}</h1>
     </div>

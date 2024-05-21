2c13a795090a29faaf70685c16f0380ec82cca68
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-21 06:58:20 -0700

    LODASH

--- src/Loadash.tsx
+++ src/Loadash.tsx
@@ -1,12 +1,38 @@
-//^ LOADASH PAGE
-// & LOADASH TUTORIAL
-// 1 - INSTALL LOADASH FROM ITS OFFICIAL WEBSITE
-// 2 -
+//^ LODASH PAGE
+import Lodash from "lodash"
+import { useState } from "react"
+
+// & LODASH TUTORIAL
+// 1 - INSTALL LODASH FROM ITS OFFICIAL WEBSITE
+// 2 - IMPORTING LODASH FROM LODASH
+// 3 - USING LODASH BY COMMANDING LODASH.{MEHTOD NAME}
+
+
+
+type productItemType = { id: number, title: string, price: number }
 
 const Loadash = () => {
+  // PRODUCTS STATE
+  const [products, setProducts] = useState([
+    { id: 1, title: "Laptop", price: 20_000_000 },
+    { id: 1, title: "Cover", price: 500_000 },
+    { id: 1, title: "Hub", price: 300_000 },
+  ])
+
+
+
+
+  // RETURN
   return (
-    <h1 className="text-slate-100 text-4xl font-bold bg-pink-500 p-4 text-center border-b-8 border-sky-300">LOADASH PAGE</h1>
+    <>
+      <h1 className="text-slate-100 text-4xl font-bold bg-pink-500 p-4 text-center border-b-8 border-sky-300">LODASH PAGE</h1>
+      <hr />
+      {/* LODASH.SUM */}
+      <h2 className='text-white text-4xl font-bold px-8 py-4 bg-zinc-800 border-b-8 border-zinc-300'>SUM : {Lodash.sum([1, 2, 3, 4])}</h2>
+      {/* LODASH.SUMBY */}
+      <h2 className='text-white text-4xl font-bold px-8 py-4 bg-zinc-800 border-b-8 border-zinc-300'>TOTAL PRICE =  {Lodash.sumBy(products, product =>  product.price ).toLocaleString()} <span className="text-lg">Tomans</span> </h2>
+    </>
   )
 }
 
-export default Loadash
+export default Loadash   

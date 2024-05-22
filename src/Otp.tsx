6708b7bfca51ef102b0124c5d8175bbbbbf644c4
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-22 02:56:09 -0700

    OTP

--- src/Otp.tsx
+++ src/Otp.tsx
@@ -1,9 +1,18 @@
+//^ OTP PAGE
 import React from 'react'
-
+//& TUTORIAL
+// 1 - WHAT IS OTP 
 const Otp = () => {
   return (
-    <div>Otp</div>
+    <div className='w-screen h-screen flex flex-col items-center justify-center'>
+      <h1 className='text-white font-extrabold text-6xl my-6'>ONE TIME PASSWORD</h1>
+      {/* //^ FORM */}
+      <form className='bg-zinc-800 w-1/3 h-1/3 rounded-xl border-4 border-zinc-100 text-white font-bold text-4xl px-4 py-10 flex flex-col space-y-10 items-center justify-center '>
+        <input type="number" className='px-6 py-4 rounded-xl font-bold text-4xl w-5/6 text-black outline-none' />
+        <button type="submit" className='bg-black px-4 py-6 rounded-xl w-5/6 hover:bg-emerald-900 duration-300'>SEND CODE</button>
+      </form>
+    </div>
   )
 }
 
-export default Otp
+export default Otp 

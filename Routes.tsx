d64769d777fc3d1ac10b8056b375c1e4fc1c416f
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-22 02:32:26 -0700

    OTP

--- Routes.tsx
+++ Routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from './src/App'
 import Lodash1 from "./src/Lodash";
 import Lodash2 from "./src/Lodash2";
+import Otp from "./src/Otp";
 
 
 
@@ -11,4 +12,5 @@ export const router = createBrowserRouter([
   { path: '/', element: <Home /> },
   { path: '/loadash1', element: <Lodash1 /> },
   { path: '/loadash2', element: <Lodash2 /> },
+  { path: '/otp', element: <Otp /> },
 ])

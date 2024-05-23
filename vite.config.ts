0d00f1a8028adc5ba7e5c7e1bcf5c11cf0859401
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-23 11:36:49 -0700

    CK

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { createRequire } from 'node:module';
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
 import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
 
+const require = createRequire(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ckeditor5({ theme: require.resolve('@ckeditor/ckeditor5-theme-lark') }],
+  plugins: [react(), ckeditor5({ theme: require.resolve('@ckeditor/ckeditor5-theme-lark') })],
 })

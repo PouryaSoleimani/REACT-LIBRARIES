e3d0b94cc6c393a1ba6411380fc90e723a8fe797
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-23 15:30:18 -0700

    CK

--- src/CkEditor.tsx
+++ src/CkEditor.tsx
@@ -8,15 +8,11 @@ const CkEditor = () => {
   return (
     <div className="w-screen h-screen">
       <h2 className='text-white text-2xl font-bold text-center py-6'>Using CKEditor&nbsp;5 build in React</h2>
-      <CKEditor
-        editor={ClassicEditor}
-        data="<h1>Hello from CKEditor&nbsp;5!</h1>"
-        onChange={(editor) => { console.log(editor) }}
-        onBlur={(event, editor) => { console.log('Blur.', editor); }}
-        onFocus={(event, editor) => { console.log('Focus.', editor); }}
-      />
+      <CKEditor editor={ClassicEditor} data="<h1>Hello from CKEditor&nbsp;5!</h1>" />
     </div>
   )
 }
 
 export default CkEditor
+
+

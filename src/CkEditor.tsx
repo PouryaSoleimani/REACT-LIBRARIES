9bf49a27270352eaeb673871b5ff6c97601eee99
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-23 11:30:04 -0700

    CK

--- src/CkEditor.tsx
+++ src/CkEditor.tsx
@@ -1,6 +1,6 @@
 //^ CK EDITOR PAGE
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import { ClassicEditor } from '@ckeditor/ckeditor5-build-classic'
 
 
 function App() {

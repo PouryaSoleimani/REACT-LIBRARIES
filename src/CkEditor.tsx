efe6ff5e58ce058ef648a022196c5d38d52ceb44
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-23 15:15:32 -0700

    CK

--- src/CkEditor.tsx
+++ src/CkEditor.tsx
@@ -1,9 +1,8 @@
 //^ CK EDITOR PAGE
+import React from 'react'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import ClassicEditor from './ckeditor.ts';
 
-
-import React from 'react'
 
 const CkEditor = () => {
   return (
@@ -11,7 +10,7 @@ const CkEditor = () => {
       <h2 className='text-white text-2xl font-bold text-center py-6'>Using CKEditor&nbsp;5 build in React</h2>
       <CKEditor
         editor={ClassicEditor}
-        data="<p>Hello from CKEditor&nbsp;5!</p>"
+        data="<h1>Hello from CKEditor&nbsp;5!</h1>"
         onReady={editor => {
           // You can store the "editor" and use when it is needed.
           console.log('Editor is ready to use!', editor);

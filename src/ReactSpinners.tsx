dafcc93ce946730200cc2095e4253f99324d4fdd
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-25 11:32:25 -0700

    REACT-SPINNERS

--- src/ReactSpinners.tsx
+++ src/ReactSpinners.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const ReactSpinners = () => {
   return (
-    <div>ReactSpinners</div>
+    <div>
+      <h1 className='text-4xl text-white bg-cyan-800 py-5 text-center font-bold'>React-Spinners</h1>
+    </div>
   )
 }
 

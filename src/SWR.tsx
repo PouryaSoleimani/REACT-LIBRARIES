79cf4717645f8ba1e47cd10f75746842f788b2ba
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-26 09:51:29 -0700

    SWR

--- src/SWR.tsx
+++ src/SWR.tsx
@@ -1,9 +1,24 @@
 import React, { useState } from 'react'
+import useSWR from 'swr'
 
+type itemType = { id: number, title: string }
 const SWR = () => {
   const [products, setProducts] = useState([])
+  function fetcher() { fetch('https://fakestoreapi.com/products').then(response => response.json()).then(result => setProducts(result)) }
+
+
+  const { error, isLoading } = useSWR('https://fakestoreapi.com/products', fetcher)
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
+
+
+
+  //^ RETURN 
   return (
-    <div>SWR</div>
+    <div className='px-4 text-white font-bold text-lg flex flex-col space-y-2 bg-zinc-900 py-2 border-8 border-zinc-900'>
+      {products.map((item: itemType) => <p>{item.id} - {item.title} </p>)}
+    </div>
   )
 }
 

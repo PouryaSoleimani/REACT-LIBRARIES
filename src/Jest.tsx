24b87c332b41245ece7db492573042601e30a028
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 07:06:47 -0700

    JEST

--- src/Jest.tsx
+++ src/Jest.tsx
@@ -2,6 +2,19 @@
 import React from 'react'
 
 const Jest = () => {
+
+  function sum(num1: number, num2: number) { return num1 + num2 }
+
+
+
+
+
+
+
+
+
+
+  //RETURN
   return (
     <div>
       <h1 className='text-4xl text-white text-center py-5 bg-zinc-900 font-bold border-b-8 border-yellow-400'>JEST PAGE</h1>

57de7ea13a766422ad7765c08eccf29b2f28c5d6
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 13:35:55 -0700

    STYLED COMPONENTS

--- src/StyledComponents2.tsx
+++ src/StyledComponents2.tsx
@@ -12,7 +12,9 @@ export const PrimaryButton = styled.button<{ $outline?: boolean; }>`
  width:16rem;
  text-align: center;
  border-radius:20px;
+ transition : all 500ms linear;
  border :10px solid ${props => props.$outline ? "hotpink" : "white"};
+  &:hover{background-color : black ; color : white;} // USING PSEUDO CLASSES IN STYLED-COMPONENTS
  `
 
 

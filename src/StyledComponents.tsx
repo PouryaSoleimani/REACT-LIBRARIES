0e3447c3dbc39aea28f25589957c7d4bb15b8ee1
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 12:49:35 -0700

    STYLED COMPONENTS

--- src/StyledComponents.tsx
+++ src/StyledComponents.tsx
@@ -3,17 +3,19 @@ import React from 'react'
 import styled from 'styled-components';
 import { PrimaryButton } from './StyledComponents2';
 
-//STYLED COMPONENTS
+//^STYLED COMPONENTS
 const Title = styled.h1`font-size: 3.5em;text-align: center;color: #124169;font-weight : 900;`
 const Wrapper = styled.section`padding : .5em ;background : #252525 ;display : flex ; flex-direction:column ; justify-content : start ; align-items : center; `
 const Button = styled.button`background-color : green ; padding : .5rem ;border-radius : 15px ;color : white ;border : 10px solid black ; font-weight:bold ; font-size:19px; margin-top:10px;`
 const Text = styled.text`color:white ;font-size : 26px ;margin-top:10px ;font-weight : bold;`
 const Circle = styled.circle`width:90px ; height : 90px ; border-radius : 100%; background-color:red; margin-top:10px; border:13px solid white ;`
-const DisabledButton = styled(PrimaryButton)`color: darkgray ; background-color : #484444;border: 10px solid black; cursor: not-allowed;` // INHERITANCE IN STYLED-COMPONENTS
 const InheritedCircle = styled(Circle)`background-color:white; border:14px solid red;`
 const InheritedText = styled(Text)`color : black; font-size : 40px; font-style:italic; background-color:white; padding:1rem 1rem;`
 const AcceptButton = styled(PrimaryButton)`background-color:green;`
 const RejectButton = styled(PrimaryButton)`background-color : darkred; border:10px solid black;`
+
+// GIVING ATTRIBUTES TO THE COMPONENTS
+const DisabledButton = styled(PrimaryButton).attrs({ disabled: true })`color: darkgray ; background-color : #484444;border: 10px solid black; cursor: not-allowed;` // INHERITANCE IN STYLED-COMPONENTS
 
 
 
@@ -39,9 +41,9 @@ const StyledComponents = () => {
         <div className='flex space-x-10 my-6'>
           <PrimaryButton onClick={() => console.log('CLICK CLICK')} $outline={true}>PRIMARY</PrimaryButton>
           <PrimaryButton onClick={() => console.log('CLICK CLICK')} $outline={false}>SECONDARY</PrimaryButton>
-          <DisabledButton disabled>DISABLED</DisabledButton>
           <AcceptButton>ACCEPT</AcceptButton>
           <RejectButton>REJECT</RejectButton>
+          <DisabledButton>DISABLED</DisabledButton>
         </div>
       </Wrapper>
     </div>

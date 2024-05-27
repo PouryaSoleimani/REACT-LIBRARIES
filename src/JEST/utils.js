ebdf9da078d67b3b2c984a553bf105787a0bb6e1
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 07:59:04 -0700

    JEST

--- src/JEST/utils.js
+++ src/JEST/utils.js
@@ -7,10 +7,11 @@ const sum = (num1, num2) => { return num1 + num2 }
 //POWER
 const power = (num1, num2) => { return num1 * num2 }
 
-
+//MINUS
+const minus = (num1, num2) => { return num1 - num2 }
 //EXPORTS
 exports.sum = sum
 exports.power = power
-
+exports.minus = minus
 
 
--- src/JEST/utils.test.js
+++ src/JEST/utils.test.js
@@ -1,11 +1,12 @@
 //^ UTILS TEST FILE
 //? IMPORTS
-const { sum, power } = require('./utils.js')
+const { sum, power, minus } = require('./utils.js')
 
 
 
 //! TESTS
 test('test sum Function', () => { expect(sum(2, 3)).toBe(5) });
 
-
 test('test power function', () => { expect(power(4, 4)).toBe(16) });
+
+test('test Minus Function', () => { expect(minus(10, 4)).toBe(6) })

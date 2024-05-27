7ccbfda6eda530409e8e98551e8a2b5213a6eda2
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 10:36:27 -0700

    STYLED COMPONENTS

--- src/StyledComponents.tsx
+++ src/StyledComponents.tsx
@@ -3,17 +3,17 @@ import React from 'react'
 import styled from 'styled-components';
 
 
-
-
-
+//STYLED COMPONENTS
+const Title = styled.h1`font-size: 3.5em;text-align: center;color: #124169;font-weight : 900;`
+const Wrapper = styled.section`padding : 4em ;background : #252525 ;display : flex ; flex-direction:column ; justify-content : center ; align-items : center; `
+const Button = styled.button`background-color : green ; padding : 1rem ;border-radius : 15px ;color : white ;border : 5px solid white ; width : 10rem;`
+const Text = styled.text`color:white ;font-size : 26px ;margin-top:10px ;font-weight : bold;`
+const Circle = styled.circle`width:110px ; height : 110px ; border-radius : 100%; background-color:red; margin-top:30px; border:12px solid white ;`
 const StyledComponents = () => {
-  const Title = styled.h1`font-size: 3.5em;text-align: center;color: #124169;font-weight : 900;`
-  const Wrapper = styled.section`padding : 4em ;background : #252525 ;display : flex ; flex-direction:column ; justify-content : center ; align-items : center  `
-  const Button = styled.button`background-color : green ; padding : 1rem ;border-radius : 15px ;color : white ;border : 5px solid white ; width : 10rem`;
-  const Text = styled.text`color:white ;font-size : 26px ;margin-top:10px ;font-weight : bold`
 
 
 
+  //COMPONENT  
   return (
     <div>
       <h1 className='bg-pink-400 text-black text-4xl text-center py-6 font-extrabold tracking-tighter w-full'>STYLED COMPONENTS</h1>
@@ -21,6 +21,7 @@ const StyledComponents = () => {
         <Title>HELLO WORLD</Title>
         <Button>CLICK ME</Button>
         <Text>THIS IS A TESTING TEXT</Text>
+        <Circle></Circle>
       </Wrapper>
     </div>
   )

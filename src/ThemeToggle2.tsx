5773cafaab07c7aecbe5807649b052d2c3c9503f
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 16:42:52 -0700

    THEME TOGGLE

--- src/ThemeToggle2.tsx
+++ src/ThemeToggle2.tsx
@@ -1,7 +1,7 @@
 //^ THEME TOGGLER 2
-import React from 'react'
+import React, { useState } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
-
+type InputTheme = { body: string, header: string, text: string, primary: string }
 
 //^ STYLED COMPONENT VARIABLES AND THEMES
 //THEMES
@@ -14,10 +14,21 @@ const Wrapper = styled.section`width : 100% ; height : 100vh ; border : 12px sol
 const Header = styled.h1`font-size : 40px ; font-weight : bold ; text-align : center ; color : ${props => props.theme.header};`
 
 const Text = styled.p`font-size : 20px; font-weight : 600 ; padding : 1rem 1rem ; margin-top : 20px ; color : ${props => props.theme.text};`
+
+const ButtonParent = styled.div`display : flex ; align-items : center ; justify-content : center; gap : 3rem; width:100%; padding : 3rem;`
+
+const DarkButton = styled.button`padding: 1rem; background-color: black ; border-radius : 30px; font-weight : bold ; font-size : 30px ; border : 17px solid #f8e643;`
+
+const LightButton = styled.button`padding : 1rem ; background-color : white ; border-radius : 30px; font-weight : bold ; font-size : 30px ; border : 17px solid hotpink;`
+
+
 //^ COMPONENT
 const ThemeToggle2 = () => {
+  //STATES AND FUNCTIONS FOR THEME TOGGLING
+  const [theme, setTheme] = useState(LightTheme)
+  function themeToggler(inputTheme) { setTheme(inputTheme) }
   return (
-    <ThemeProvider theme={LightTheme}>
+    <ThemeProvider theme={theme}>
       <Wrapper>
         <Header>THEME TOGGLE PAGE</Header>
         <Text> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, consequuntur dolorem ex sit odit exercitationem ipsa voluptatibus aspernatur quia suscipit alias eum architecto ullam, obcaecati nostrum mollitia tempore molestiae voluptates.
@@ -27,6 +38,10 @@ const ThemeToggle2 = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, explicabo unde! Vel omnis unde aliquam quia quidem? At earum a nesciunt, labore, numquam minus quod odit soluta libero iusto explicabo.
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel doloribus, hic provident totam nesciunt beatae saepe maxime consequatur sed ut, quasi quod recusandae tempora dolorum nostrum voluptate praesentium repellat velit.
         </Text>
+        <ButtonParent>
+          <LightButton>☀</LightButton>
+          <DarkButton>🌙</DarkButton>
+        </ButtonParent>
       </Wrapper>
     </ThemeProvider>
   )

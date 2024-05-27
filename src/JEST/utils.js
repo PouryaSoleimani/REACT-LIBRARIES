e30ea906e2ad518e9af703ebdb8835b20851b5f0
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 07:44:14 -0700

    JEST

---
+++ src/JEST/utils.js
@@ -0,0 +1,3 @@
+//^ UTILS.JS FILE
+const sum = (num1, num2) => { return num1 + num2 }
+module.exports = sum
---
+++ src/JEST/utils.test.js
@@ -0,0 +1,5 @@
+const sum = require('./utils')
+
+test('test sum Function', () => {
+  expect(sum(2, 3)).toBe(5)
+})

177a4d195f79e5be25f6977044a9559c7f676f43
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 11:31:56 -0700

    STYLED COMPONENTS

--- src/StyledComponents2.tsx
+++ src/StyledComponents2.tsx
@@ -4,13 +4,15 @@ import styled from 'styled-components';
 
 
 export const PrimaryButton = styled.button<{ $primary?: boolean; }>`
-background-color : ${props => props.$primary ? "white" : 'hotpink'};
+ background-color : ${props => props.$primary ? "white" : 'hotpink'};
  font-size:30px ;
  margin-top:20px ; 
  color:${props => props.$primary ? "black" : "white"} ;
-font-weight: bold;
- padding:1rem 3rem;
+ font-weight: bold;
+ width:20rem;
+ text-align: center;
  border-radius:20px;
+ border :10px solid ${props => props.$primary ? "hotpink" : "white"};
  `
 
 

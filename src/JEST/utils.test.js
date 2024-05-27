8d17127da98ce7c71a632b9232519389a9e30a94
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 08:08:34 -0700

    JEST

--- src/JEST/utils.test.js
+++ src/JEST/utils.test.js
@@ -14,4 +14,6 @@ test('test Minus Function', () => { expect(minus(10, 4)).toBe(6) })
 test('test Clone Function', () => {
   const array = [1, 2, 3]
   expect(clone(array)).toStrictEqual(array)
+  // expect(clone(array)).toEqual(array) --> THIS IS JUST THE SAME AS {toStrictEqual}
+  // expect(clone(array)).toBe(array) --> IF WE DIDNT USE THE {SPREAD SYNTAX} [...array] , WE COULD USE THIS
 })

d45d2f16540bb34b3a57c35b8ff7511e0c8916fd
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 16:26:14 -0700

    STYLED COMPONENTS

--- TUTORIALS/Theme-Toggle-Tutorial.md
+++ TUTORIALS/Theme-Toggle-Tutorial.md
@@ -4,3 +4,5 @@
 2 - SECOND we wrap the whole component with the <ThemeProvider></ThemeProvider> Component 
 
 3 - THIRD we define two different themes : `DARKTHEME` and `LIGHTTHEME`
+
+4 - FOURTH we make our own styled Components Like : Wrapper , Title , Text , ...
--- src/ThemeToggle2.tsx
+++ src/ThemeToggle2.tsx
@@ -1,8 +1,24 @@
+//^ THEME TOGGLER 2
 import React from 'react'
 import styled, { ThemeProvider } from 'styled-components'
+
+
+//^ STYLED COMPONENT VARIABLES AND THEMES
+//THEMES
+const DarkTheme = { body: "#303030", header: "#9f9999", text: '#d4d3d3', darkPrimary: '#f8e643', }
+
+const LightTheme = { body: "#fafafa", header: "#131313", text: "#2e2d2d", lightPrimary: "hotpink" }
+
+
+
+
+
+//^ COMPONENT
 const ThemeToggle2 = () => {
   return (
-    <div>ThemeToggle2</div>
+    <ThemeProvider theme={LightTheme}>
+
+    </ThemeProvider>
   )
 }
 

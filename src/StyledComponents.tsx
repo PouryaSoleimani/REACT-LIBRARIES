2358009b9d47157d0182336893b4b64684030875
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 12:34:03 -0700

    STYLED COMPONENTS

--- src/StyledComponents.tsx
+++ src/StyledComponents.tsx
@@ -10,7 +10,7 @@ const Button = styled.button`background-color : green ; padding : 1rem ;border-r
 const Text = styled.text`color:white ;font-size : 26px ;margin-top:10px ;font-weight : bold;`
 const Circle = styled.circle`width:90px ; height : 90px ; border-radius : 100%; background-color:red; margin-top:10px; border:13px solid white ;`
 const DisabledButton = styled(PrimaryButton)`color: darkgray ; background-color : #484444;border: 10px solid black; cursor: not-allowed;` // INHERITANCE IN STYLED-COMPONENTS
-
+const InheritedCircle = styled(Circle)`background-color:white; border:14px solid red;`
 
 
 //COMPONENT  
@@ -21,13 +21,20 @@ const StyledComponents = () => {
       <h1 className='bg-pink-400 text-black text-4xl text-center py-6 font-extrabold tracking-tighter w-full'>STYLED COMPONENTS</h1>
       <Wrapper>
         <Title>HELLO WORLD</Title>
+        <div className='flex space-x-6'>
         <Button>CLICK ME</Button>
         <Button>CLICK ME</Button>
+        </div>
         <Text>THIS IS A TESTING TEXT</Text>
-        <Circle></Circle>
+        <div className='flex space-x-6 mt-4'>
+          <Circle></Circle>
+          <InheritedCircle></InheritedCircle>
+        </div>
+        <div className='flex space-x-10 my-6'>
         <PrimaryButton onClick={() => console.log('CLICK CLICK')} $outline={true}>PRIMARY</PrimaryButton>
         <PrimaryButton onClick={() => console.log('CLICK CLICK')} $outline={false}>SECONDARY</PrimaryButton>
         <DisabledButton disabled>DISABLED</DisabledButton>
+        </div>
       </Wrapper>
     </div>
   )

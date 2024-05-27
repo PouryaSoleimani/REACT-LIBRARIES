6f2b139b658a6f95267577a997822517c4010a6a
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 10:12:58 -0700

    STYLED COMPONENTS

--- src/StyledComponents.tsx
+++ src/StyledComponents.tsx
@@ -4,10 +4,15 @@ import styled from 'styled-components';
 
 
 
+
+
 const StyledComponents = () => {
-  const Title = styled.h1`font-size: 3.5em; text-align: center;color: #124169; font-weight : 900`
-  const Wrapper = styled.section`padding : 4em ; background : #171717 ; display : flex ; flex-direction:column ; justify-content : center ; align-items : center`
-  const Button = styled.button`background-color : green ; padding : 1rem ; border-radius : 15px ; color : white ; border : 5px solid white ; width : 10rem`
+  const Title = styled.h1`font-size: 3.5em;text-align: center;color: #124169;font-weight : 900;`
+  const Wrapper = styled.section`padding : 4em ;background : #252525 ;display : flex ; flex-direction:column ; justify-content : center ; align-items : center  `
+  const Button = styled.button`background-color : green ; padding : 1rem ;border-radius : 15px ;color : white ;border : 5px solid white ; width : 10rem`;
+  const Text = styled.text`color:white ;font-size : 26px ;margin-top:10px ;font-weight : bold`
+
+
 
   return (
     <div>
@@ -15,6 +20,7 @@ const StyledComponents = () => {
       <Wrapper>
         <Title>HELLO WORLD</Title>
         <Button>CLICK ME</Button>
+        <Text>THIS IS A TESTING TEXT</Text>
       </Wrapper>
     </div>
   )

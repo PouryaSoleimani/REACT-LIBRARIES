bfe2e616f7a264d33c3503cb9310105aace28b1f
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 11:25:36 -0700

    STYLED COMPONENTS

--- src/StyledComponents.tsx
+++ src/StyledComponents.tsx
@@ -6,7 +6,7 @@ import { PrimaryButton } from './StyledComponents2';
 //STYLED COMPONENTS
 const Title = styled.h1`font-size: 3.5em;text-align: center;color: #124169;font-weight : 900;`
 const Wrapper = styled.section`padding : 4em ;background : #252525 ;display : flex ; flex-direction:column ; justify-content : center ; align-items : center; `
-const Button = styled.button`background-color : green ; padding : 1rem ;border-radius : 15px ;color : white ;border : 5px solid white ; width : 10rem; font-weight:bold ; font-size:19px;`
+const Button = styled.button`background-color : green ; padding : 1rem ;border-radius : 15px ;color : white ;border : 10px solid black ; width : 10rem; font-weight:bold ; font-size:19px; margin-top:10px;`
 const Text = styled.text`color:white ;font-size : 26px ;margin-top:10px ;font-weight : bold;`
 const Circle = styled.circle`width:110px ; height : 110px ; border-radius : 100%; background-color:red; margin-top:30px; border:12px solid white ;`
 //COMPONENT  
@@ -24,6 +24,7 @@ const StyledComponents = () => {
         <Text>THIS IS A TESTING TEXT</Text>
         <Circle></Circle>
         <PrimaryButton $primary={true}>PRIMARY</PrimaryButton>
+        <PrimaryButton $primary={false}>SECONDARY</PrimaryButton>
       </Wrapper>
     </div>
   )

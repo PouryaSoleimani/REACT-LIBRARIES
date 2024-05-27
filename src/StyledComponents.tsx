096ed560683df249c9ec696517879e9129d6ad39
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 09:36:57 -0700

    STYLED COMPONENTS

--- src/StyledComponents.tsx
+++ src/StyledComponents.tsx
@@ -1,9 +1,27 @@
+//& STYLED-COMPONENTS
 import React from 'react'
+import styled from 'styled-components';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const StyledComponents = () => {
+
   return (
     <div>
       <h1 className='bg-pink-400 text-black text-4xl text-center py-6 font-extrabold tracking-tighter w-full'>STYLED COMPONENTS</h1>
+
     </div>
   )
 }

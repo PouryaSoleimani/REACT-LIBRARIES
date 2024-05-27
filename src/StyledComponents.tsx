a7dee459878abe0abb6fded76b09e96c6f0e8be7
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 11:47:05 -0700

    STYLED COMPONENTS

--- src/StyledComponents.tsx
+++ src/StyledComponents.tsx
@@ -6,7 +6,7 @@ import { PrimaryButton } from './StyledComponents2';
 //STYLED COMPONENTS
 const Title = styled.h1`font-size: 3.5em;text-align: center;color: #124169;font-weight : 900;`
 const Wrapper = styled.section`padding : .5em ;background : #252525 ;display : flex ; flex-direction:column ; justify-content : start ; align-items : center; `
-const Button = styled.button`background-color : green ; padding : 1rem ;border-radius : 15px ;color : white ;border : 10px solid black ; width : 10rem; font-weight:bold ; font-size:19px; margin-top:10px;`
+const Button = styled.button`background-color : green ; padding : 1rem ;border-radius : 15px ;color : white ;border : 10px solid black ; font-weight:bold ; font-size:19px; margin-top:10px;`
 const Text = styled.text`color:white ;font-size : 26px ;margin-top:10px ;font-weight : bold;`
 const Circle = styled.circle`width:90px ; height : 90px ; border-radius : 100%; background-color:red; margin-top:10px; border:13px solid white ;`
 //COMPONENT  

589c80e6af84e53bfbd85b5cc77f894b0295fe22
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 18:23:58 -0700

    STYLED COMPONENTS

--- src/StyledComponents3.tsx
+++ src/StyledComponents3.tsx
@@ -2,13 +2,15 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
+//STYLED COMPONENTS
+const Wrapper = styled.div`display : flex ; flex-direction : column ; justify-content : center ; align-items : center ; gap : 1rem ; padding : 2rem ; border:10px solid lightsalmon; width : 100% ; height : 100vh;`
+const Header = styled.h1`color : salmon; font-size : 50px ;`
 //COMPONENT
 const StyledComponents3 = () => {
   return (
-    <div>
-      
-    </div>
+    <Wrapper>
+
+    </Wrapper>
   )
 }
 

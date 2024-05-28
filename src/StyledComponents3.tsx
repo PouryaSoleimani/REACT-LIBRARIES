ed909cebc60fc26f17b3925f3b5f58a39cab28d3
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-27 18:41:40 -0700

    STYLED COMPONENTS

--- src/StyledComponents3.tsx
+++ src/StyledComponents3.tsx
@@ -1,15 +1,21 @@
 //^ STYLED COMPONENTS 3 PAGE
 import React from 'react'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 //STYLED COMPONENTS
 const Wrapper = styled.div`display : flex ; flex-direction : column ; justify-content : start ; align-items : center ; gap : 1rem ; padding : .2rem ; border:10px solid lightsalmon; width : 100% ; height : 100vh;`
 const Header = styled.h1`color : #a51201; font-size : 50px ; text-align: center ; padding: 1rem ; font-weight : bold ;`
 const Text = styled.p`color : lightgray ; font-size : 22px ; padding : .2rem 2rem ; text-align : justify;`
 const ButtonsParent = styled.div`display : flex ; justify-content : center ; align-items : center ; gap : 2rem ; margin-top : 3rem;`
-const AcceptButton = styled.button`padding : .2rem 2rem ; width: 15rem; background-color : green ; font-size :24px ; font-weight : bold ; border-radius : 15px ;border : 5px solid currentColor; color : white; &:hover{transform : scale(1.2)}; transition : all 300ms linear;`
+const AcceptButton = styled.button`padding : .2rem 2rem ; width: 15rem; background-color : green ; font-size :24px ; font-weight : bold ; border-radius : 15px ; color : white; &:hover{transform : scale(1.2)}; transition : all 300ms linear;`
 const RejectButton = styled(AcceptButton)`background-color : darkred;`
 const DisabledButton = styled(AcceptButton).attrs({ disabled: true })`background-color:gray ; color : darkgray ; cursor: not-allowed;`
+
+//GLOBAL STYLES
+const GlobalStyles = createGlobalStyle`button { border : 10px solid white ; border-radius : 20px;}`
+
+
+
 
 //COMPONENT
 const StyledComponents3 = () => {

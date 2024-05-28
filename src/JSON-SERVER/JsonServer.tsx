e94ca9eac1ae1add14d030462d62810b74b1090c
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-28 09:45:26 -0700

    JSON SERVER

--- src/JSON-SERVER/JsonServer.tsx
+++ src/JSON-SERVER/JsonServer.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 //STYLED COMPONENTS ========================================================================================================================================
-const Wrapper = styled.div`display : flex ; align-items : center ; justify-content : center ;`
+const Wrapper = styled.div`display : flex ; align-items : center ; justify-content : center ; flex-direction : column ;`
 const PageTitle = styled.h1`text-align : center ; font-size : 30px ; color : lime ; border-bottom : 10px solid lime ; background-color : #202020 ; padding : 1rem; width : 100%; font-weight : bold;`
-
+const DatasWrapper = styled(Wrapper)`width : 90% ; height : 90vh ;`
 
 
 
@@ -13,6 +13,9 @@ const JsonServer = () => {
   return (
     <Wrapper>
       <PageTitle>JSON SERVER</PageTitle>
+      <DatasWrapper>
+      
+      </DatasWrapper>
     </Wrapper>
   )
 }

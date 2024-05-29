20a0f4d04a815bf10e1dadba55cbf868394be1ac
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-29 11:11:58 -0700

    JSON SERVER

--- src/JSON-SERVER/JsonServer.tsx
+++ src/JSON-SERVER/JsonServer.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import useSWR from 'swr'
 type ProductItemType = { id: string, title: string, price: number }
 
 //STYLED COMPONENTS ========================================================================================================================================

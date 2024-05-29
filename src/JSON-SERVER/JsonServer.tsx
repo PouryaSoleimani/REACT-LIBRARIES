e9e6bc561fcdaced9807242a761c1d71c8c2298f
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-29 14:39:40 -0700

    JSON SERVER

--- src/JSON-SERVER/JsonServer.tsx
+++ src/JSON-SERVER/JsonServer.tsx
@@ -9,8 +9,12 @@ const Wrapper = styled.div`display : flex ; align-items : center ; justify-conte
 const PageTitle = styled.h1`text-align : center ; font-size : 30px ; color : lime ; border-bottom : 10px solid lime ; background-color : #202020 ; padding : 1rem; width : 100%; font-weight : bold;`
 const DatasWrapper = styled(Wrapper)`width : 100% ; padding : .5rem 1rem; display : flex ; flex-direction : column ; align-items : start ; justify-content : start; border-bottom : 10px solid white;`
 const Product = styled.p`font-size:22px ; color : #e1e1e1; line-height:40px; font-weight : 800;`
-const FormWrapper = styled.div`display:flex ; align-items: center;justify-content : center ; padding : 20rem ; border-bottom : 10px solid white;`
-const Form = styled.form`display : flex ; flex-direction : column ; align-items:center ; justify-content : start; padding : 1rem; border : 5px solid lime ; border-radius : 20px;`
+const FormWrapper = styled.div`display:flex ; align-items: center;justify-content : start ; padding : 20rem ; border-bottom : 10px solid white;`
+const Form = styled.form`display : flex ; flex-direction : column ; align-items:center ; justify-content : start; padding : 1rem; border : 5px solid lime ; border-radius : 20px; gap: .5rem;`
+const FormTitle = styled(PageTitle)`border-radius : 20px;`
+const Button = styled.button`padding : 1rem 2rem; border-radius : 20px ; border: 5px solid lime ; background-color : #202020; color : white ; font-size : 26px; font-weight : 900;`
+
+
 
 // COMPONENT ===============================================================================================================================================
 const JsonServer = () => {
@@ -29,7 +33,12 @@ const JsonServer = () => {
       </DatasWrapper>
 
       <FormWrapper>
-
+        <Form>
+          <FormTitle>FORM</FormTitle>
+          <input className='p-4 text-xl text-black rounded-xl' type="text" placeholder='First Name' />
+          <input className='p-4 text-xl text-black rounded-xl' type="text" placeholder='Last Name' />
+          <Button>SUBMIT</Button>
+        </Form>
       </FormWrapper>
     </Wrapper>
   )
